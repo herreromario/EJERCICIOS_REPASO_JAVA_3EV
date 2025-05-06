ec68ca94e0025668ebea27b2d6f02b79858dd2dc
herreromario <always.haxe@gmail.com>
2025-05-06 21:20:32 +0200

    EJERCICIO 3 COMPLETO

---
+++ src/ejercicio3/Reserva.java
@@ -0,0 +1,47 @@
+package ejercicio3;
+import java.io.*;
+
+public class Reserva implements Serializable {
+
+	private int numHabitacion;
+	private String nombreCliente;
+	private String fechaReserva;
+	
+	// CONSTRUCTOR
+	public Reserva(int numHabitacion, String nombreCliente, String fechaReserva) {
+		this.numHabitacion = numHabitacion;
+		this.nombreCliente = nombreCliente;
+		this.fechaReserva = fechaReserva;
+	}
+	
+	// GETTERS
+	public int getNumHabitacion() {
+		return numHabitacion;
+	}
+	
+	public String getNombreCliente() {
+		return nombreCliente;
+	}	
+	
+	public String getFechaReserva() {
+		return fechaReserva;
+	}
+	
+	// SETTERS
+	public void setNumHabitacion(int numHabitacion) {
+		this.numHabitacion = numHabitacion;
+	}
+	
+	public void setNombreCliente(String nombreCliente) {
+		this.nombreCliente = nombreCliente;
+	}
+	
+	public void setFechaReserva(String fechaReserva) {
+		this.fechaReserva = fechaReserva;
+	}
+	
+	@Override
+	public String toString() {
+		return "Habitacion: " + numHabitacion + ", a nombre de: " + nombreCliente + ", para el dia: " + fechaReserva;	
+	}
+}
---
+++ src/ejercicio3/Reservable.java
@@ -0,0 +1,12 @@
+package ejercicio3;
+
+import java.util.ArrayList;
+
+public interface Reservable {
+
+	public void anyadirReserva(ArrayList<Reserva> listaReservas);
+	
+	public void cancelarReserva(ArrayList<Reserva> listaReservas);
+	
+	public void listarReservas(ArrayList<Reserva> listaReservas);
+}
